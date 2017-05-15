c3ed93b0218e3096c3f9e64015e3f4574f2ec5d6
GavrilM <ghmoniaga@gmail.com>
2017-05-14 22:21:24 -0700

    tabs on group

--- web/src/components/group.vue
+++ web/src/components/group.vue
@@ -1,5 +1,12 @@
 <template>
 	<div class="group">
+		<input type="radio" id="recent" v-model="type" value="recent" class="hidden" checked="checked">
+		<input type="radio" id="hottest" v-model="type" value="hottest" class="hidden">
+		<div class="tabs">
+			<label for="recent" :class="{ active: type === 'recent' }" >Recent</label>
+			<label for="hottest" :class="{ active: type === 'hottest' }">Hottest</label>
+		</div>
+	
 		<Feed :roasts="roasts"></Feed>
 	</div>
 </template>
@@ -14,6 +21,7 @@ import Feed from '@/components/Feed'
 		},
 		data() {
 			return {
+				type: 'recent',
 				amount: 20
 			}
 		},
@@ -29,9 +37,30 @@ import Feed from '@/components/Feed'
 	}
 </script>
 
-<style>
+<style scoped lang="less">
 	.group{
 		height: inherit;
 		overflow-y: scroll;
+	}
+	.hidden{
+		display: none;
+	}
+	.tabs{
+		display:flex;
+		width:100%;
+		height: 60px;
+		label{
+			display: flex;
+			justify-content: center;
+			align-items:center;
+			width:100%;
+			font-fmaily: 'Poppins';
+			font-size: 1.2em;
+			font-weight: 700;
+		}
+		.active{
+			background-color: #d75339;
+			color:white;
+		}
 	}
 </style>
--- web/src/layouts/login.vue
+++ web/src/layouts/login.vue
@@ -87,79 +87,82 @@
 </script>
 
 <style lang="less">
-.btn{
-	display: flex;
-	justify-content: center;
-	align-items:center;
-	width:100%;
-	font-family: 'Poppins';
-	font-size: 1.2em;
-	font-weight: 700;
-	cursor: pointer;
-}
-.active{
-	background-color: #d75339;
-	color:white;
-}
-.tabs{
-	display:flex;
-	width:500px;
-	height: 40px;
-	margin: 15px auto 0;
-	border: 1px solid #d75339;
-	border-left: none;
-	label{	
-		border-left: 1px solid #d75339;
-	}
-}
-.hasCode{
-	width:500px;
-	height: 40px;
-	margin: 0 auto;
-	border: 1px solid #d75339;
-	border-top: none;
-	&:hover{
-		.active;
-	}
-}
-.login-form{
-	form {
-		padding: 20px;
-		/*height: 30vh;*/
-		width: 50vw;
-		min-width: 300px;
-		min-height: 300px;
-		max-width: 500px;
-		margin: 0 auto;
-	}
-	input[type="text"], input[type="password"]{
-		margin: 10px 0;
-		padding: 5px 5px;
-		font-size: 1.5em;
-		border: 1px solid grey;
-		border-radius: 3px;
-		transition: border 500ms;
-	} 
-	input[type="text"]:focus, input[type="password"]:focus{
-		border: 1px solid lightblue;
-		border-radius: 3px;
-	}
-	input[type="submit"]{
-		background: none;
-		border: 1px solid grey;
-		color: #E80A0A;
-		font-size: 1.5em; 
-		border-radius: 3px;
+.login{
+
+	.btn{
+		display: flex;
+		justify-content: center;
+		align-items:center;
+		width:100%;
+		font-family: 'Poppins';
+		font-size: 1.2em;
+		font-weight: 700;
 		cursor: pointer;
-		padding: 2px 10px;
-		margin-top: 10px;
 	}
-	.invalid{
-		border-color: red !important;
+	.active{
+		background-color: #d75339;
+		color:white;
 	}
-	label{
-		color: red;
-		display:block;
+	.tabs{
+		display:flex;
+		width:500px;
+		height: 40px;
+		margin: 15px auto 0;
+		border: 1px solid #d75339;
+		border-left: none;
+		label{	
+			border-left: 1px solid #d75339;
+		}
+	}
+	.hasCode{
+		width:500px;
+		height: 40px;
+		margin: 0 auto;
+		border: 1px solid #d75339;
+		border-top: none;
+		&:hover{
+			.active;
+		}
+	}
+	.login-form{
+		form {
+			padding: 20px;
+			/*height: 30vh;*/
+			width: 50vw;
+			min-width: 300px;
+			min-height: 300px;
+			max-width: 500px;
+			margin: 0 auto;
+		}
+		input[type="text"], input[type="password"]{
+			margin: 10px 0;
+			padding: 5px 5px;
+			font-size: 1.5em;
+			border: 1px solid grey;
+			border-radius: 3px;
+			transition: border 500ms;
+		} 
+		input[type="text"]:focus, input[type="password"]:focus{
+			border: 1px solid lightblue;
+			border-radius: 3px;
+		}
+		input[type="submit"]{
+			background: none;
+			border: 1px solid grey;
+			color: #E80A0A;
+			font-size: 1.5em; 
+			border-radius: 3px;
+			cursor: pointer;
+			padding: 2px 10px;
+			margin-top: 10px;
+		}
+		.invalid{
+			border-color: red !important;
+		}
+		label{
+			color: red;
+			display:block;
+		}
 	}
 }
 </style>
